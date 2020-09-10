82454361b4fd6219bac39fb091d671eb938202e6
AbhinavJoel <abhijoe20@gmail.com>
2020-09-10 23:02:50 +0530

    sentences stored as arrays

---
+++ RDIP/Codes/script.js
@@ -0,0 +1,131 @@
+let sen11 = "John ate an apple before afternoon";
+sen11 = sen11.split(" ");
+
+let sen12 = "before afternoon John ate an apple";
+sen12 = sen12.split(" ");
+
+let sen13 = "John before afternoon ate an apple";
+sen13 = sen13.split(" ");
+
+let sen21 = "some students like to study in the night";
+sen21 = sen21.split(" ");
+
+let sen22 = "at night some students like to study";
+sen22 = sen22.split(" ");
+
+let sen31 = "John and Mary went to church";
+sen31 = sen31.split(" ");
+
+let sen32 = "Mary and John went to church";
+sen32 = sen32.split(" ");
+
+let sen41 = "John went to church after eating";
+sen41 = sen41.split(" ");
+
+let sen42 = "after eating John went to church";
+sen42 = sen42.split(" ");
+
+let sen43 = "John after eating went to church";
+sen43 = sen43.split(" ");
+
+let sen51 = "did he go to market";
+sen51 = sen51.split(" ");
+
+let sen52 = "he did go to market";
+sen52 = sen52.split(" ");
+
+let sen61 = "the woman who called my sister sells cosmetics";
+sen61 = sen61.split(" ");
+
+let sen62 = "the woman who sells cosmetics called my sister";
+sen62 = sen62.split(" ");
+
+let sen63 = "my sister who sells cosmetics called the woman";
+sen63 = sen63.split(" ");
+
+let sen64 = "my sister who called the woman sells cosmetics";
+sen64 = sen64.split(" ");
+
+let sen71 = "John goes to the library and studies";
+sen71 = sen71.split(" ");
+
+let sen72 = "John studies and goes to the library";
+sen72 = sen72.split(" ");
+
+let sen81 = "John ate an apple so did she";
+sen81 = sen81.split(" ");
+
+let sen82 = "she ate an apple so did John";
+sen82 = sen82.split(" ");
+
+let sen91 = "the teacher returned the book after she noticed the error";
+sen91 = sen91.split(" ");
+
+let sen92 = "the teacher noticed the error after she returned the book";
+sen92 = sen92.split(" ");
+
+let sen93 = "after the teacher returned the book she noticed the error";
+sen93 = sen93.split(" ");
+
+let sen94 = "after the teacher noticed the error she returned the book";
+sen94 = sen94.split(" ");
+
+let sen95 = "she returned the book after the teacher noticed the error";
+sen95 = sen95.split(" ");
+
+let sen96 = "she noticed the error after the teacher returned the book";
+sen96 = sen96.split(" ");
+
+let sen97 = "after she returned the book the teacher noticed the error";
+sen97 = sen97.split(" ");
+
+let sen98 = "after she noticed the error the teacher returned the book";
+sen98 = sen98.split(" ");
+
+let sen01 = "I told her that I bought a book yesterday";
+sen01 = sen01.split(" ");
+
+let sen02 = "I told her yesterday that I bought a book";
+sen02 = sen02.split(" ");
+
+let sen03 = "yesterday I told her that I bought a book";
+sen03 = sen03.split(" ");
+
+let sen04 = "I bought a book that I told her yesterday";
+sen04 = sen04.split(" ");
+
+let sen05 = "I bought a book yesterday that I told her";
+sen05 = sen05.split(" ");
+
+let sen06 = "yesterday I bought a book that I told her";
+sen06 = sen06.split(" ");
+
+
+
+let sen1a = "राम और श्याम बाजार गयें";
+sen1a = sen1a.split(" ");
+console.log(sen1a)
+
+let sen1b = "राम और श्याम गयें बाजार";
+sen1b = sen1b.split(" ");
+console.log(sen1a)
+
+let sen1c = "बाजार गयें राम और श्याम";
+sen1c = sen1c.split(" ");
+console.log(typeof(sen1a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
