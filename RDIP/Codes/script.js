d0a6a07a6f3cebc8c439e6a6d16650bdf252ad53
AbhinavJoel <abhijoe20@gmail.com>
2020-09-13 23:56:03 +0530

    language sel dropdown made

--- RDIP/Codes/script.js
+++ RDIP/Codes/script.js
@@ -1,131 +1,279 @@
-let sen11 = "John ate an apple before afternoon";
-sen11 = sen11.split(" ");
+// let sen11 = "John ate an apple before afternoon";
+// sen11 = sen11.split(" ");
 
-let sen12 = "before afternoon John ate an apple";
-sen12 = sen12.split(" ");
+// let sen12 = "before afternoon John ate an apple";
+// sen12 = sen12.split(" ");
 
-let sen13 = "John before afternoon ate an apple";
-sen13 = sen13.split(" ");
+// let sen13 = "John before afternoon ate an apple";
+// sen13 = sen13.split(" ");
 
-let sen21 = "some students like to study in the night";
-sen21 = sen21.split(" ");
+// let sen21 = "some students like to study in the night";
+// sen21 = sen21.split(" ");
 
-let sen22 = "at night some students like to study";
-sen22 = sen22.split(" ");
+// let sen22 = "at night some students like to study";
+// sen22 = sen22.split(" ");
 
-let sen31 = "John and Mary went to church";
-sen31 = sen31.split(" ");
+// let sen31 = "John and Mary went to church";
+// sen31 = sen31.split(" ");
 
-let sen32 = "Mary and John went to church";
-sen32 = sen32.split(" ");
+// let sen32 = "Mary and John went to church";
+// sen32 = sen32.split(" ");
 
-let sen41 = "John went to church after eating";
-sen41 = sen41.split(" ");
+// let sen41 = "John went to church after eating";
+// sen41 = sen41.split(" ");
 
-let sen42 = "after eating John went to church";
-sen42 = sen42.split(" ");
+// let sen42 = "after eating John went to church";
+// sen42 = sen42.split(" ");
 
-let sen43 = "John after eating went to church";
-sen43 = sen43.split(" ");
+// let sen43 = "John after eating went to church";
+// sen43 = sen43.split(" ");
 
-let sen51 = "did he go to market";
-sen51 = sen51.split(" ");
+// let sen51 = "did he go to market";
+// sen51 = sen51.split(" ");
 
-let sen52 = "he did go to market";
-sen52 = sen52.split(" ");
+// let sen52 = "he did go to market";
+// sen52 = sen52.split(" ");
 
-let sen61 = "the woman who called my sister sells cosmetics";
-sen61 = sen61.split(" ");
+// let sen61 = "the woman who called my sister sells cosmetics";
+// sen61 = sen61.split(" ");
 
-let sen62 = "the woman who sells cosmetics called my sister";
-sen62 = sen62.split(" ");
+// let sen62 = "the woman who sells cosmetics called my sister";
+// sen62 = sen62.split(" ");
 
-let sen63 = "my sister who sells cosmetics called the woman";
-sen63 = sen63.split(" ");
+// let sen63 = "my sister who sells cosmetics called the woman";
+// sen63 = sen63.split(" ");
 
-let sen64 = "my sister who called the woman sells cosmetics";
-sen64 = sen64.split(" ");
+// let sen64 = "my sister who called the woman sells cosmetics";
+// sen64 = sen64.split(" ");
 
-let sen71 = "John goes to the library and studies";
-sen71 = sen71.split(" ");
+// let sen71 = "John goes to the library and studies";
+// sen71 = sen71.split(" ");
 
-let sen72 = "John studies and goes to the library";
-sen72 = sen72.split(" ");
+// let sen72 = "John studies and goes to the library";
+// sen72 = sen72.split(" ");
 
-let sen81 = "John ate an apple so did she";
-sen81 = sen81.split(" ");
+// let sen81 = "John ate an apple so did she";
+// sen81 = sen81.split(" ");
 
-let sen82 = "she ate an apple so did John";
-sen82 = sen82.split(" ");
+// let sen82 = "she ate an apple so did John";
+// sen82 = sen82.split(" ");
 
-let sen91 = "the teacher returned the book after she noticed the error";
-sen91 = sen91.split(" ");
+// let sen91 = "the teacher returned the book after she noticed the error";
+// sen91 = sen91.split(" ");
 
-let sen92 = "the teacher noticed the error after she returned the book";
-sen92 = sen92.split(" ");
+// let sen92 = "the teacher noticed the error after she returned the book";
+// sen92 = sen92.split(" ");
 
-let sen93 = "after the teacher returned the book she noticed the error";
-sen93 = sen93.split(" ");
+// let sen93 = "after the teacher returned the book she noticed the error";
+// sen93 = sen93.split(" ");
 
-let sen94 = "after the teacher noticed the error she returned the book";
-sen94 = sen94.split(" ");
+// let sen94 = "after the teacher noticed the error she returned the book";
+// sen94 = sen94.split(" ");
 
-let sen95 = "she returned the book after the teacher noticed the error";
-sen95 = sen95.split(" ");
+// let sen95 = "she returned the book after the teacher noticed the error";
+// sen95 = sen95.split(" ");
 
-let sen96 = "she noticed the error after the teacher returned the book";
-sen96 = sen96.split(" ");
+// let sen96 = "she noticed the error after the teacher returned the book";
+// sen96 = sen96.split(" ");
 
-let sen97 = "after she returned the book the teacher noticed the error";
-sen97 = sen97.split(" ");
+// let sen97 = "after she returned the book the teacher noticed the error";
+// sen97 = sen97.split(" ");
 
-let sen98 = "after she noticed the error the teacher returned the book";
-sen98 = sen98.split(" ");
+// let sen98 = "after she noticed the error the teacher returned the book";
+// sen98 = sen98.split(" ");
 
-let sen01 = "I told her that I bought a book yesterday";
-sen01 = sen01.split(" ");
+// let sen01 = "I told her that I bought a book yesterday";
+// sen01 = sen01.split(" ");
 
-let sen02 = "I told her yesterday that I bought a book";
-sen02 = sen02.split(" ");
+// let sen02 = "I told her yesterday that I bought a book";
+// sen02 = sen02.split(" ");
 
-let sen03 = "yesterday I told her that I bought a book";
-sen03 = sen03.split(" ");
+// let sen03 = "yesterday I told her that I bought a book";
+// sen03 = sen03.split(" ");
 
-let sen04 = "I bought a book that I told her yesterday";
-sen04 = sen04.split(" ");
+// let sen04 = "I bought a book that I told her yesterday";
+// sen04 = sen04.split(" ");
 
-let sen05 = "I bought a book yesterday that I told her";
-sen05 = sen05.split(" ");
+// let sen05 = "I bought a book yesterday that I told her";
+// sen05 = sen05.split(" ");
 
-let sen06 = "yesterday I bought a book that I told her";
-sen06 = sen06.split(" ");
+// let sen06 = "yesterday I bought a book that I told her";
+// sen06 = sen06.split(" ");
 
+// let sen1a = "राम और श्याम बाजार गयें";
+// sen1a = sen1a.split(" ");
 
+// let sen1b = "राम और श्याम गयें बाजार";
+// sen1b = sen1b.split(" ");
 
-let sen1a = "राम और श्याम बाजार गयें";
-sen1a = sen1a.split(" ");
-console.log(sen1a)
+// let sen1c = "बाजार गयें राम और श्याम";
+// sen1c = sen1c.split(" ");
 
-let sen1b = "राम और श्याम गयें बाजार";
-sen1b = sen1b.split(" ");
-console.log(sen1a)
+// let sen1d = "गयें बाजार राम और श्याम";
+// sen1d = sen1d.split(" ");
 
-let sen1c = "बाजार गयें राम और श्याम";
-sen1c = sen1c.split(" ");
-console.log(typeof(sen1a))
+// let sen2a = "राम सोया और श्याम भी";
+// sen2a = sen2a.split(" ");
 
+// let sen2b = "श्याम सोया और राम भी";
+// sen2b = sen2b.split(" ");
 
+// let sen2c = "सोया श्याम और राम भी";
+// sen2c = sen2c.split(" ");
 
+// let sen2d = "सोया राम और श्याम भी";
+// sen2d = sen2d.split(" ");
 
+// let sen3a = "मैंने उसे बताया कि राम सो रहा है";
+// sen3a = sen3a.split(" ");
 
+// let sen3b = "मैंने उसे बताया कि सो रहा है राम";
+// sen3b = sen3b.split(" ");
 
+// let sen3c = "उसे मैंने बताया कि राम सो रहा है";
+// sen3c = sen3c.split(" ");
 
+// let sen3d = "उसे मैंने बताया कि सो रहा है राम";
+// sen3d = sen3d.split(" ");
 
+// let sen3e = "मैंने बताया उसे कि राम सो रहा है";
+// sen3e = sen3e.split(" ");
 
+// let sen3f = "मैंने बताया उसे कि सो रहा है राम";
+// sen3f = sen3f.split(" ");
 
+// let sen3g = "उसे बताया मैंने कि राम सो रहा है";
+// sen3g = sen3g.split(" ");
 
+// let sen3h = "उसे बताया मैंने कि सो रहा है राम";
+// sen3h = sen3h.split(" ");
 
+// let sen3i = "बताया मैंने उसे कि राम सो रहा है";
+// sen3i = sen3i.split(" ");
 
+// let sen3j = "बताया मैंने उसे कि सो रहा है राम";
+// sen3j = sen3j.split(" ");
 
+// let sen3k = "बताया उसे मैंने कि राम सो रहा है";
+// sen3k = sen3k.split(" ");
 
+// let sen3l = "बताया उसे मैंने कि सो रहा है राम";
+// sen3l = sen3l.split(" ");
+
+// let sen4a = "राम खाकर सोया";
+// sen4a = sen4a.split(" ");
+
+// let sen4b = "खाकर राम सोया";
+// sen4b = sen4b.split(" ");
+
+// let sen4c = "राम सोया खाकर";
+// sen4c = sen4c.split(" ");
+
+// let sen4d = "खाकर सोया राम";
+// sen4d = sen4d.split(" ");
+
+// let sen4e = "सोया राम खाकर";
+// sen4e = sen4e.split(" ");
+
+// let sen4f = "सोया खाकर राम";
+// sen4f = sen4f.split(" ");
+
+// let sen5a = "बिल्लियों को मारकर कुत्ता सो गया";
+// sen5a = sen5a.split(" ");
+
+// let sen5b = "मारकर बिल्लियों को कुत्ता सो गया";
+// sen5b = sen5b.split(" ");
+
+// let sen5c = "बिल्लियों को मारकर सो गया कुत्ता";
+// sen5c = sen5c.split(" ");
+
+// let sen5d = "मारकर बिल्लियों को सो गया कुत्ता";
+// sen5d = sen5d.split(" ");
+
+// let sen5e = "कुत्ता सो गया बिल्लियों को मारकर";
+// sen5e = sen5e.split(" ");
+
+// let sen5f = "कुत्ता सो गया मारकर बिल्लियों को";
+// sen5f = sen5f.split(" ");
+
+// let sen5g = "सो गया कुत्ता बिल्लियों को मारकर";
+// sen5g = sen5g.split(" ");
+
+// let sen5h = "सो गया कुत्ता मारकर बिल्लियों को";
+// sen5h = sen5h.split(" ");
+
+// let sen6a = "एक लाल किताब वहाँ है";
+// sen6a = sen6a.split(" ");
+
+// let sen6b = "एक लाल किताब है वहाँ";
+// sen6b = sen6b.split(" ");
+
+// let sen6c = "वहाँ है एक लाल किताब";
+// sen6c = sen6c.split(" ");
+
+// let sen6d = "है वहाँ एक लाल किताब";
+// sen6d = sen6d.split(" ");
+
+// let sen7a = "एक बड़ी सी किताब वहाँ है";
+// sen7a = sen7a.split(" ");
+
+// let sen7b = "एक बड़ी सी किताब है वहाँ";
+// sen7b = sen7b.split(" ");
+
+// let sen7c = "बड़ी सी एक किताब वहाँ है";
+// sen7c = sen7c.split(" ");
+
+// let sen7d = "बड़ी सी एक किताब है वहाँ";
+// sen7d = sen7d.split(" ");
+
+// let sen7e = "वहाँ है एक बड़ी सी किताब";
+// sen7e = sen7e.split(" ");
+
+// let sen7f = "वहाँ है बड़ी सी एक किताब";
+// sen7f = sen7f.split(" ");
+
+// let sen7g = "है वहाँ एक बड़ी सी किताब";
+// sen7g = sen7g.split(" ");
+
+let sen7h = "है वहाँ बड़ी सी एक किताब";
+sen7h = sen7h.split(" ");
+
+function selection() {
+	if (document.getElementById("languages").selectedIndex == 0) {
+		alert("Select a Language")
+		location.reload()
+	}
+	if (document.getElementById("languages").selectedIndex == 1) {
+		document.getElementById("text1").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words";
+		document.getElementById("text2").innerHTML = "(select the buttons in proper order)"
+	}
+	if(document.getElementById("languages").selectedIndex == 2) {
+		document.getElementById("text1").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words";
+		document.getElementById("text2").innerHTML = "(select the buttons in proper order)"
+	}
+
+}
+
+function alertForText() {
+	alert("choose a lang")
+}
+function shuffle(arr) {
+	var curIndex = arr.length, temp, randIndex;
+
+	while (0 !== curIndex) {
+
+		randIndex = Math.floor(Math.rand() * curIndex);
+		curIndex -= 1;
+
+		temp = arr[curIndex];
+		arr[curIndex] = arr[randIndex];
+		arr[randIndex] = temp;
+	}
+
+	return arr;
+}
+
+// Used like so
+shuffle(sen7h);
+console.log(sen7h);
 
